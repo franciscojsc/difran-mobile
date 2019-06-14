2551a94f7401ff9ea06a338cf87ae2cc42cf021e
Francisco Chaves <francisco.jschaves@gmail.com>
2019-06-14 00:06:56 -0300

    Criando interface de Login

---
+++ app/src/main/java/br/com/franciscochaves/difran/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package br.com.franciscochaves.difran.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import br.com.franciscochaves.difran.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastroUsuario(View view) {
+    }
+}
--- app/src/main/java/br/com/franciscochaves/difran/activity/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/difran/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package br.com.franciscochaves.difran;
+package br.com.franciscochaves.difran.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.franciscochaves.difran.R;
 
 public class MainActivity extends AppCompatActivity {
 
